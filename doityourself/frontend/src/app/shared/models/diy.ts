c90dda4f243ff71987824ab0b5e61bae20fc34b3
Luanna Virginia <86988841+luandradevirginiamelo@users.noreply.github.com>
2024-07-26 21:16:34 +0100

    FinalProject4.final

---
+++ doityourself/frontend/src/app/shared/models/diy.ts
@@ -0,0 +1,9 @@
+export class diy{
+    id!:string;
+    name!:string;
+    tags?: string[];
+    favorite!:boolean;
+    stars!: number;
+    imageUrl!: string;
+    content!: string;
+  }
---
+++ doityourself/frontend/src/data.ts
@@ -0,0 +1,15 @@
+import {diy} from './app/shared/models/diy';
+
+
+export const sample_datas: diy[] = [
+    {
+      id: '1',
+      name: 'Service 1',
+      favorite: false,
+      stars: 4.5,
+      imageUrl: 'assets/imagem3.jpg',
+      tags: ['PLUMBER', 'Example'],
+      content: 'Bla bla bla'
+    },
+
+]
